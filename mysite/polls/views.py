40313816a0f76b6c40104437d9e72662ce6f85d7
devmcdonald <dpm360@nyu.edu>
2024-03-02 15:30:27 -0500

    modified:   mysite/polls/admin.py 	modified:   mysite/polls/migrations/0001_initial.py 	modified:   mysite/polls/models.py 	modified:   mysite/polls/urls.py 	modified:   mysite/polls/views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,6 +5,7 @@ from .models import Question, Choice
 from django.urls import reverse
 from django.views import generic
 
+
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
@@ -22,7 +23,7 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
-    
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -30,10 +31,14 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, "polls/detail.html", {
-            "question": question,
-            "error_message": "You didn't select a choice.",
-        })
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
     else:
         selected_choice.votes += 1
         selected_choice.save()
